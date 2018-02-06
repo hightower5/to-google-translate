be84dd51f6c836e6fec2cb31dba87be278452051
hightower5 <hightower500@gmail.com>
2018-02-06 14:26:50 +0100

    Update options.js
    German 'settings saved'
    
    Is it possible to detect user lang?

--- options.js
+++ options.js
@@ -31,7 +31,7 @@ function saveOptions(e) {
             chrome.i18n.getMessage('contextMenuTitleTranslate', [pageLang.value, userLang.value]));
         updateContextMenuTitle('tts', 
             chrome.i18n.getMessage('contextMenuTitleTextToSpeech', ttsLang.value));
-        showMessage('Settings saved');
+        showMessage('Einstellungen gespeichert');
         
         if (enableTT.checked == false) {
             removeContextMenu('translate');
